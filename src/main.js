845630adae5d2957fe43fb9e4454c1cdd1cb37f8
Ra <rareq1987@gmail.com>
2015-11-17 23:39:29 +0100

    add animation

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ export function configure(aurelia) {
     .developmentLogging();
 
   //Uncomment the line below to enable animation.
-  //aurelia.use.plugin('aurelia-animator-css');
+  aurelia.use.plugin('aurelia-animator-css');
 
   //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
   //aurelia.use.plugin('aurelia-html-import-template-loader')
